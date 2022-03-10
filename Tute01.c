a00f3a5a0097114e7c2bed5084be7725f19e165c
Sonali  Fernando <100190036+IT21180248@users.noreply.github.com>
2022-03-10 09:26:49 +0000

    Tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2;
+  float avg;
+  
+  printf("input mark1 =");
+  scanf("%d", &mark1);
+
+  printf("input mark2 =");
+  scanf("%d",&mark2);
+
+  avg=(mark1+mark2)/2;
+  
+  printf("avarage is : %.2f",avg);
+
   
   return 0;
 }
